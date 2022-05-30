bfaced9355b24c15f49cc7bcf9289c6c81580a2b
Matthew T Leiman <matthewtleiman@gmail.com>
2022-05-30 11:32:08 -0600

    database schema created

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initalizes the database
+db = SQLAlchemy()   
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'whateverthesecretkey'
+                                            # F string is string interpolation
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)    
     
     from .views import views
     from .auth import auth
---
+++ website/models..py
@@ -0,0 +1,28 @@
+from time import timezone
+from . import db
+# Module from flask that helps users login
+from flask_login import UserMixin
+from sqlalchemy.sql import func 
+
+# Defining the schemea for the database layout. Just like EF core in C#
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    # Whenever a new note is created, sqlalchemy func will time stamp with current time automatically when inserted
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # Note must belong to a user. Setting relationship to user using foreign key
+    # lowercase user is the name of the table in the database. One to many relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+class User (db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+                                        # No user can have the same email as another user. 
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    # Sets the relationship to the notes table by referencing the table name.
+    # Foreign key lowercase, Referencing the table is Uppercase
+    notes = db.relationship('Note')
+    

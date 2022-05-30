6c7d2571d65d74a5b3a64a6970c338f75b158961
Matthew T Leiman <matthewtleiman@gmail.com>
2022-05-30 11:14:34 -0600

    flashed messages setup

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from xmlrpc.client import boolean
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-
+# methods signifies that this route accept GET and POST requests. Will only be defaulted to GET if not specified.
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -12,6 +13,24 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    # flash is  a built in function that will display a message to the user provided by flask
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else: 
+            # add user to the database
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
